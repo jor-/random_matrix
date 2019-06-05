7b5dbdbf1c804fd848960eb23178d5882374aaf0
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-06-05 19:36:01 +0200

    ENH: random_matrix.random: orthogonal_matrix added

--- random_matrix/random.py
+++ random_matrix/random.py
@@ -42,3 +42,42 @@ def lower_triangle_matrix(n, dense=True, complex_values=False, random_value_func
     else:
         A = scipy.sparse.tril(A)
     return A
+
+
+def orthogonal_matrix(n):
+    """
+    reference:
+    G.W. Stewart,
+    The efficient generation of random orthogonal matrices
+    with an application to condition estimators,
+    SIAM J. Numer. Anal., 17 (1980), 403-409.
+    """
+
+    A = np.eye(n)
+    d = np.zeros(n)
+
+    def sign_without_zero(v):
+        return np.sign(v) + (v == 0)
+
+    for k in range(n - 2, -1, -1):
+        # Generate random Householder transformation.
+        x = np.random.randn(n - k)
+        s = np.linalg.norm(x)
+        sgn = sign_without_zero(x[0])
+        s = sgn * s
+        d[k] = -sgn
+        x[0] = x[0] + s
+        beta = s * x[0]
+        # Apply the transformation to A.
+        y = x.T @ A[k:n]
+        A[k:n] = A[k:n] - np.outer(x, y / beta)
+
+    for i in range(0, n - 1):
+        A[i] = d[i] * A[i]
+
+    sgn = sign_without_zero(np.random.randn(1))
+    A[n - 1] = A[n - 1] * sgn
+
+    assert np.allclose(A @ A.T, np.eye(n))
+    assert np.allclose(A.T @ A, np.eye(n))
+    return A

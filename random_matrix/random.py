c9486e8dff5ba6d7d8efcc7e9f061f987707f5ae
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-06-07 12:24:02 +0200

    ENH: random_matrix.random: correlation_matrix_with_eigenvalues added

--- random_matrix/random.py
+++ random_matrix/random.py
@@ -91,3 +91,60 @@ def symmetric_matrix_with_eigenvalues(eigenvalues):
     A = (A + A.T) / 2
     assert np.all(A == A.T)
     return A
+
+
+def correlation_matrix_with_eigenvalues(eigenvalues):
+    """"
+    reference:
+    P. I. Davies and N. J. Higham,
+    Numerically stable generation of correlation matrices and their factors,
+    BIT, 40 (2000), pp. 640-651.
+    """
+
+    eigenvalues = np.asanyarray(eigenvalues)
+    if np.any(eigenvalues < 0):
+        raise ValueError(f'Invalid eigenvalues {eigenvalues}. Eigenvalues must be greater or equal to zero.')
+    assert eigenvalues.ndim == 1
+
+    D = np.diag(eigenvalues)
+    n = eigenvalues.size
+    Q = orthogonal_matrix(n)
+    A = Q @ D @ Q.T
+
+    a = np.diag(A)
+    y = np.where(a < 1)[0]
+    z = np.where(a > 1)[0]
+
+    while y.size > 0 and z.size > 0:
+        i = y[np.random.choice(y.size)]
+        j = z[np.random.choice(z.size)]
+        if i > j:
+            tmp = i
+            i = j
+            j = tmp
+
+        alpha = (A[i, j]**2 - (a[i] - 1) * (a[j] - 1))**0.5
+        sign = np.sign(A[i, j]) + A[i, j] == 0
+        t = (A[i, j] + sign * alpha) / (a[j] - 1)
+        if np.random.choice([True, False]):
+            t = (a[i] - 1) / ((a[j] - 1) * t)
+        c = (1 + t**2)**(-0.5)
+        s = t * c
+
+        A[:, (i, j)] = A[:, (i, j)] @ np.array([[c, s], [-s, c]])
+        A[(i, j), :] = np.array([[c, -s], [s, c]]) @ A[(i, j), :]
+
+        A[i, i] = 1
+
+        a = np.diag(A)
+        y = np.where(a < 1)[0]
+        z = np.where(a > 1)[0]
+
+    for i in range(n):
+        A[i, i] = 1
+
+    A = (A + A.conj().transpose()) / 2
+
+    assert np.all(A >= -1)
+    assert np.all(A <= 1)
+    return A
